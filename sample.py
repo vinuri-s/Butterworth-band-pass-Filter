735d0c064f8679241f91b324baff7850def6770d
vinuri-s <piyathilakev@gmail.com>
2021-11-09 12:56:02 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,23 +8,25 @@ lowcut = 10.0
 highcut = 150.0
 FRAME_RATE = 600
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
+def butter_bandpass(lowcut, highcut, FRAME_RATE, order=9):
+    nyq = 0.5 * FRAME_RATE
     low = lowcut / nyq
     high = highcut / nyq
     b, a = butter(order, [low, high], btype='band')
     return b, a
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+def butter_bandpass_filter(data, lowcut, highcut, FRAME_RATE, order=9):
+    b, a = butter_bandpass(lowcut, highcut, FRAME_RATE, order=order)
     y = lfilter(b, a, data)
     #y = filtfilt(b, a, data)
     return y
 
 def bandpass_filter(buffer):
-    return butter_bandpass_filter(buffer, lowcut, highcut, FRAME_RATE, order=6)
+    return butter_bandpass_filter(buffer, lowcut, highcut, FRAME_RATE, order=9)
 
-samplerate, data = wavfile.read('sample.wav')
+samplerate, data = wavfile.read('11.wav')
+t = np.arange(len(data)) / float(samplerate);  # Retrieving Time
+#data = data/max(data);  # Normalize Audio Data
 filtered = np.apply_along_axis(bandpass_filter, 0, data).astype('int16')
 wavfile.write('filtered_sample.wav', samplerate, filtered)
 
@@ -32,28 +34,46 @@ wavfile.write('filtered_sample.wav', samplerate, filtered)
 # Plot the frequency response for a few different orders.
 plt.figure(1)
 plt.clf()
-for order in [3, 6, 9,12]:
+for order in [3, 6, 9, 12]:
         b, a = butter_bandpass(lowcut, highcut, FRAME_RATE, order=order)
         w, h = freqz(b, a, worN=2000)
         plt.plot((FRAME_RATE* 0.5 / np.pi) * w, abs(h), label="order = %d" % order)
 
 plt.plot([0, 0.5 * FRAME_RATE], [np.sqrt(0.5), np.sqrt(0.5)],'--', label='sqrt(0.5)')
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Gain')
+plt.ylabel('Gain(dB)')
 plt.grid(True)
 plt.legend(loc='best')
+plt.savefig('plot1.png', dpi=100)  # Saving plot as PNG image
 
 # Filter a noisy signal.
-t = np.arange(len(data)) / float(samplerate);  # Retrieving Time
 plt.figure(2)
 plt.clf()
 plt.plot(t, data, label='Noisy signal')
-
-y = butter_bandpass_filter(data, lowcut, highcut, FRAME_RATE, order=9)
-plt.plot(t, y, label='Filtered signal (%g Hz)')
+#plt.plot(t, filtered, label='Filtered signal')
 plt.xlabel('time (seconds)')
 plt.grid(True)
 plt.axis('tight')
 plt.legend(loc='upper left')
+plt.savefig('plot2.png', dpi=100)  # Saving plot as PNG image
+
+plt.figure(3)
+plt.clf()
+plt.plot(t, filtered, label='Filtered signal')
+plt.xlabel('time (seconds)')
+plt.grid(True)
+plt.axis('tight')
+plt.legend(loc='upper left')
+plt.savefig('plot3.png', dpi=100)  # Saving plot as PNG image
+
+plt.figure(4)
+plt.clf()
+plt.plot(t, data, label='Noisy signal')
+plt.plot(t, filtered, label='Filtered signal')
+plt.xlabel('time (seconds)')
+plt.grid(True)
+plt.axis('tight')
+plt.legend(loc='upper left')
+plt.savefig('plot4.png', dpi=100)  # Saving plot as PNG image
 
 plt.show()
